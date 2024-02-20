ab159270fb3eb2236619de9baf44b27d53b06e1a
LaxusxN <luiscalagua2294@gmail.com>
2024-02-20 12:42:26 -0500

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Cursos, Contact } from "./components";
+// import { Cursos, Contact } from "./components";
 
 function App() {
   return (
     <div className="flex flex-col p-32 gap-8 bg-[#8DA8BF] text-[#FFFFFF]">
       <div className="w-auto">
-        <Cursos />
+        HolaMundo
+        {/* <Cursos /> */}
       </div>
-      <Contact />
+      {/* <Contact /> */}
     </div>
   );
 }
