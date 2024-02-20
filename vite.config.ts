06454dc770edda4a02fcbdd2e123ac5d9e64700d
LaxusxN <luiscalagua2294@gmail.com>
2024-02-20 10:08:11 -0500

    viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "kaludemy",
-  // base: 'https://LaxusxN.github.io/kaludemy'
+  base: "https://LaxusxN.github.io/kaludemy",
+  build: {
+    outDir: "dist",
+  },
 });
